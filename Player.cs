e3f2a0103f35e7365f8b48ad825652fe37b13bc3
reaped-git <reaperrighty@gmail.com>
2025-11-07 18:55:35 +0700

    Battleship C#

---
+++ Player.cs
@@ -0,0 +1,22 @@
+﻿namespace BattleshipCS;
+
+public abstract class Player
+{
+    public string Name { get; }
+    public GameBoard MyBoard { get; }
+    public GameBoard? EnemyBoard { get; protected set; }
+
+    protected Player(string name, int boardSize)
+    {
+        Name = name;
+        MyBoard = new GameBoard(boardSize);
+    }
+
+    public void SetEnemyBoard(GameBoard board)
+    {
+        EnemyBoard = board;
+    }
+
+    public abstract void PlaceShips();
+    public abstract (int, int) MakeMove();
+}
