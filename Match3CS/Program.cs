b140c242d4ca8bdc5461f8d279e932d1d369e9f4
reaped-git <reaperrighty@gmail.com>
2025-11-13 22:41:02 +0700

    Match3GameCS

---
+++ Match3CS/Program.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using System;
+
+namespace Match3GameCS
+{
+    /// <summary>
+    /// Главный класс приложения - точка входа
+    /// Инициализирует Avalonia UI и запускает главное окно
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Точка входа в приложение
+        /// </summary>
+        [STAThread]
+        public static void Main(string[] args) => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+
+        /// <summary>
+        /// Настройка и конфигурация Avalonia приложения
+        /// </summary>
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()    // Автоматическое определение платформы
+                .LogToTrace();          // Логирование для отладки
+    }
+}
